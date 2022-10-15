0ee498d096076ed2286f52f33c06387432243ff7
infernal <nikita.dolgopolov@gmail.com>
2022-10-15 12:51:05 +0300

    rework lte_helper

--- app/helpers/lte_helper.rb
+++ app/helpers/lte_helper.rb
@@ -3,12 +3,11 @@ module LteHelper
     options[:text] ||= ""
     options[:path] ||= "#"
     options[:class] ||= "btn-default"
-    icon = "<i class='fas #{options[:icon]}'></i> ".html_safe if options[:icon].present?
 
     if options[:method].present?
-      link_to(icon.to_s + options[:text], options[:path], method: options[:method], class: "btn #{options[:class]}").html_safe
+      link_to(options[:text], options[:path], method: options[:method], class: "btn #{options[:class]}").html_safe
     else
-      link_to(icon.to_s + options[:text], options[:path], class: "btn #{options[:class]}").html_safe
+      link_to(options[:text], options[:path], class: "btn #{options[:class]}").html_safe
     end
   end
 
@@ -16,20 +15,68 @@ module LteHelper
     options[:title] ||= ""
     options[:size] ||= 1
 
-    out = ""
-    out << %(
-            <section class="content-header"> \
-            <div class="container-fluid"> \
-                <div class="row mb-2"> \
-                    <div class="col-sm-6"> \
-                        <h#{options[:size]}>#{options[:title]}</h#{options[:size]}> \
-                    </div> \
-                    <div class="col-sm-6"></div> \
-                </div> \
-            </div> \
-            </section> \
-    )
+    %(
+      <section class="content-header"> \
+      <div class="container-fluid"> \
+          <div class="row mb-2"> \
+              <div class="col-sm-6"> \
+                  <h#{options[:size]}>#{options[:title]}</h#{options[:size]}> \
+              </div> \
+              <div class="col-sm-6"></div> \
+          </div> \
+      </div> \
+      </section> \
+    ).html_safe
+  end
 
+  ###--Кнопки--###
+  def draw_edit_button(options)
+    disabled = options[:disabled].present? ? " disabled" : ""
+    link_to(icon('fas', 'edit', 'Редактировать'), options[:path], class: "btn btn-primary#{disabled}")
+  end
+
+  def draw_back_button(options)
+    disabled = options[:disabled].present? ? " disabled" : ""
+    link_to(icon('fas', 'arrow-left', 'Назад'), options[:path], class: "btn btn-secondary#{disabled}")
+  end
+
+  def draw_delete_button(options)
+    disabled = options[:disabled].present? ? " disabled" : ""
+    options[:confirm_text] ||= "Вы уверены?"
+    link_to(icon('fas', 'trash-alt', 'Удалить'), options[:path], method: :delete, data: {confirm: options[:confirm_text]},
+            class: "btn btn-danger float-right#{disabled}")
+  end
+
+  def draw_save_button(options = {})
+    # для кнопки за пределами формы необходимы параметры type="button", onclick="submit()"
+    options[:type] ||= "submit"
+    options[:name] ||= "button"
+    form = options[:form].present? ? %( form="#{options[:form]}") : ""
+    id = options[:id].present? ? %( id="#{options[:id]}") : ""
+    disabled = options[:disabled].present? ? " disabled" : ""
+    onclick = options[:onclick].present? ? %( onclick="#{options[:onclick]}" ) : ""
+
+    out = %(
+            <button type="#{options[:type]}" class="btn btn-success float-right"#{form}#{id} name="#{options[:name]}"
+            #{onclick}#{disabled}><i class="fas fa-save"></i> Сохранить</button>
+    )
     out.html_safe
   end
+
+  def draw_new_button(options)
+    disabled = options[:disabled].present? ? " disabled" : ""
+
+    if options[:button].present?
+      %(
+      #{link_to(%(
+          <button type="button" class="btn btn-sm btn-light">
+            <i class="fas fa-plus"></i> Добавить
+          </button>
+        ).html_safe, options[:path])}
+      ).html_safe
+    else
+      link_to(icon('fas', 'plus', 'Добавить'), options[:path], class: "btn btn-light btn-sm#{disabled}")
+    end
+  end
+  ###----###
 end
