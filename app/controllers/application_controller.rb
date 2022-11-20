100144564577a5160297b1ae1e4b807bafe059e6
infernal <nikita.dolgopolov@gmail.com>
2022-11-20 22:23:58 +0300

    redirect to logon when not authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "Для выполнения данного действия необходимо авторизоваться."
+    redirect_back(fallback_location: root_path)
+  end
 end
