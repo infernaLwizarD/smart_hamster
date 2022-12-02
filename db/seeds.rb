145c76a1e3f5c6ac73458e4156bf6702296f45ee
infernal <nikita.dolgopolov@gmail.com>
2022-12-03 01:46:52 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+def create_roles
+  return unless Role.all.empty?
+
+  Role.create!([{ label: 'Админ' }, { label: 'Родитель' }, { label: 'Хомяк' }])
+end
+
+def create_users
+  User.create!(
+    role_id: Role::ADMIN,
+    username: 'admin',
+    password: '1q2w3e4r',
+    email: 'smart_hamster_admin@admin.com',
+    confirmed_at: Time.zone.now
+  )
+end
+
+create_roles
+create_users
